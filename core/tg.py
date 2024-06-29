9d9c4b115a8a7b4145873674f3fe2385d0956178
Grisha <grisha765@tuta.io>
2024-06-29 17:49:32 +0300

    [r] refactor tg

--- core/tg.py
+++ core/tg.py
@@ -1,144 +1,119 @@
 import os
+import re
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from youtube.download import download_audio, download_video, get_video_info
 from config.config import Config
 from config import logging_config
-
 logging = logging_config.setup_logging(__name__)
 
+cache = {}
 url_list = {}
-media_cache = {}
-channel_id = Config.tg_channel_id
 
 app = Client("bot", api_id=Config.tg_id, api_hash=Config.tg_hash, bot_token=Config.tg_token)
 
-@app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply("Привет! Отправь мне ссылку на видео.")
-    logging.debug(f"User {message.from_user.id} started the bot")
-
-async def send_to_channel(file_path: str):
-    file_extension = os.path.splitext(file_path)[1].lower()
-    try:
-        logging.debug(f"Отправка {file_path} в канал {channel_id}")
-        if file_extension in ['.mp4', '.webm', '.mkv']:
-            file_send = await app.send_video(channel_id, file_path)
-            logging.debug(f"Видео {file_path} успешно отправлено в канал {channel_id}")
-        elif file_extension in ['.mp3', '.wav', '.ogg']:
-            file_send = await app.send_audio(channel_id, file_path)
-            logging.debug(f"Аудио {file_path} успешно отправлено в канал {channel_id}")
-        else:
-            logging.warning(f"Формат файла {file_extension} не поддерживается для отправки.")
-        os.remove(file_path)
-        return file_send
-    except Exception as e:
-        logging.error(f"Ошибка при отправке файла: {e}")
-        return None
+def create_quality_buttons(qualities):
+    buttons = []
+    for quality in qualities:
+        quality_number = re.match(r'(\d+)', quality).group(1)
+        buttons.append([InlineKeyboardButton(quality, callback_data=f'quality_{quality_number}')])
+    buttons.append([InlineKeyboardButton("Audio", callback_data='audio')])
+    return InlineKeyboardMarkup(buttons)
 
 @app.on_message(filters.text & filters.private)
-async def handle_link(client, message):
-    try:
-        url = message.text
-        url_list[message.from_user.id] = url
-        logging.debug(f"URL получен от пользователя {message.from_user.id}: {url}")
+async def handle_message(client, message):
+    text = message.text
+    user_id = message.from_user.id
+    logging.debug(f"Received message: {text}")
 
-        send_message = await message.reply("Получение информации об видео...")
-        if url.startswith("https://"):
-            info = await get_video_info(url)
-            logging.debug(f"Информация о видео получена: {info}")
+    url_pattern = r'(https?://(?:www\.|m\.)?youtube\.com/watch\?v=[\w-]+|https?://youtu\.be/[\w-]+)'
+    urls = re.findall(url_pattern, text)
+    
+    logging.debug(f"Found URLs: {urls}")
 
-            name = info["name"]
-            duration = info["duration"]
-            description = info["description"]
-            author = info["author"]
-            date = info["date"]
-            qualities = info["qualities"]
-            thumbnail = info["thumbnail"]
+    if urls:
+        for url in urls:
+            url_list[user_id] = url  # Save the URL for the user
+            video_info = await get_video_info(url)
+            logging.debug(f"Video info for {url}: {video_info}")
+            
+            qualities = video_info.get('qualities', [])
+            if qualities:
+                reply_markup = create_quality_buttons(qualities)
+                message_text = f"""
+**Название**: {video_info['name']}
+**Автор**: {video_info['author']}
+**Дата выхода**: {video_info['date']}
+**Продолжительность**: {video_info['duration']}
+                """
+                await message.reply_photo(photo=video_info['thumbnail'], caption=message_text, reply_markup=reply_markup)
+    else:
+        logging.debug("No URLs found in the message.")
 
-            buttons = [InlineKeyboardButton(f"{quality}", callback_data=f"quality_{quality.split(' - ')[0]}") for quality in qualities]
-            buttons.append(InlineKeyboardButton("Audio", callback_data="audio"))
+@app.on_callback_query(filters.regex(r'^quality_\d+$'))
+async def handle_quality_selection(client, callback_query: CallbackQuery):
+    quality = callback_query.data.split('_')[1]
+    user_id = callback_query.from_user.id
+    logging.debug(f"Selected quality: {quality}")
 
-            reply_markup = InlineKeyboardMarkup([[button] for button in buttons])
-            if thumbnail:
-                await send_message.reply_photo(photo=thumbnail, caption=f"{name}\nОписание:\n{description}\nАвтор: {author}\nДата выхода: {date}\nПродолжительность: {duration}\nВыберите качество:", reply_markup=reply_markup)
-                await send_message.delete()
-            else:
-                await send_message.edit_text(
-                    f"Название: {name}\nОписание:\n```{description}```\nАвтор: {author}\nДата выхода: {date}\nПродолжительность: {duration}\nВыберите качество:", reply_markup=reply_markup
-                )
-        else:
-            await send_message.edit_text("Пожалуйста, отправьте корректную ссылку, начинающуюся с 'https://'.")
-            logging.warning(f"Некорректный URL получен от пользователя {message.from_user.id}: {url}")
+    url = url_list.get(user_id)
+    if not url:
+        await callback_query.answer("URL not found.")
+        return
 
-    except Exception as e:
-        logging.error(f"Ошибка при обработке ссылки: {e}")
+    cache_key = (url, quality)
+    if cache_key in cache:
+        await callback_query.answer("Sending cached video...")
+        cached_message_id = cache[cache_key]
+        await app.forward_messages(
+            chat_id=callback_query.message.chat.id, 
+            from_chat_id=cached_message_id[0], 
+            message_ids=cached_message_id[1],
+            drop_author=True
+        )
+    else:
+        await callback_query.answer(f"Selected quality: {quality}")
+        file_name = await download_video(url, quality)
+        logging.debug(f"Downloaded file: {file_name}")
+        sent_message = await app.send_video(
+            chat_id=callback_query.message.chat.id, 
+            video=file_name, 
+            caption=f"{quality}"
+        )
+        cache[cache_key] = (callback_query.message.chat.id, sent_message.id)
+        os.remove(file_name)
 
-@app.on_callback_query(filters.regex(r"^quality_(.*)$"))
-async def handle_quality_choice(client, callback_query):
-    try:
-        quality = callback_query.data.split("_")[1]
-        url = url_list[callback_query.from_user.id]
-        logging.debug(f"Пользователь {callback_query.from_user.id} выбрал качество {quality} для URL {url}")
+@app.on_callback_query(filters.regex(r'^audio$'))
+async def handle_audio_selection(client, callback_query: CallbackQuery):
+    user_id = callback_query.from_user.id
+    logging.debug("Audio option selected")
 
-        cache_key = f"{url}_{quality}"
-        if cache_key in media_cache:
-            message_id = media_cache[cache_key]
-            logging.debug(f"Видео в качестве {quality} для URL {url} найдено в кеше")
-            await app.forward_messages(callback_query.message.chat.id, channel_id, message_id, drop_author=True)
-        else:
-            info = await get_video_info(url)
-            qualities = [q.split(' - ')[0] for q in info["qualities"]]
+    url = url_list.get(user_id)
+    if not url:
+        await callback_query.answer("URL not found.")
+        return
 
-            if quality in qualities:
-                info_send = await callback_query.message.reply_text(f"Видео в качестве {quality} загружается.")
-                await callback_query.answer()
-                file = await download_video(url, quality)
-                message_sent = await send_to_channel(file)
-                if message_sent:
-                    media_cache[cache_key] = message_sent.id
-                    logging.debug(f"Видео в качестве {quality} для URL {url} добавлено в кеш")
-                    await info_send.delete()
-                    await app.forward_messages(callback_query.message.chat.id, channel_id, message_sent.id, drop_author=True)
-            else:
-                await callback_query.message.edit_text("Выбранное качество недоступно!")
-                logging.warning(f"Выбранное качество {quality} недоступно для URL {url}")
-
-        del url_list[callback_query.from_user.id]
-        logging.debug(f"URL удален из списка запросов для пользователя {callback_query.from_user.id}")
-
-    except Exception as e:
-        logging.error(f"Ошибка при обработке выбора качества: {e}")
-        await callback_query.answer()
-
-@app.on_callback_query(filters.regex(r"^audio$"))
-async def handle_audio_choice(client, callback_query):
-    try:
-        url = url_list[callback_query.from_user.id]
-        logging.debug(f"Пользователь {callback_query.from_user.id} выбрал аудио для URL {url}")
-
-        cache_key = f"{url}_audio"
-        if cache_key in media_cache:
-            message_id = media_cache[cache_key]
-            logging.debug(f"Аудио для URL {url} найдено в кеше")
-            await app.forward_messages(callback_query.message.chat.id, channel_id, message_id, drop_author=True)
-        else:
-            info_send = await callback_query.message.reply_text("Аудио загружается.")
-            await callback_query.answer()
-            file = await download_audio(url)
-            message_sent = await send_to_channel(file)
-            if message_sent:
-                media_cache[cache_key] = message_sent.id
-                logging.debug(f"Аудио для URL {url} добавлено в кеш")
-                await info_send.delete()
-                await app.forward_messages(callback_query.message.chat.id, channel_id, message_sent.id, drop_author=True)
-
-        del url_list[callback_query.from_user.id]
-        logging.debug(f"URL удален из списка запросов для пользователя {callback_query.from_user.id}")
-
-    except Exception as e:
-        logging.error(f"Ошибка при обработке выбора аудио: {e}")
-        await callback_query.answer()
+    cache_key = (url, 'audio')
+    if cache_key in cache:
+        await callback_query.answer("Sending cached audio...")
+        cached_message_id = cache[cache_key]
+        await app.forward_messages(
+            chat_id=callback_query.message.chat.id, 
+            from_chat_id=cached_message_id[0], 
+            message_ids=cached_message_id[1],
+            drop_author=True
+        )
+    else:
+        await callback_query.answer("Audio option selected")
+        file_name = await download_audio(url)
+        logging.debug(f"Downloaded file: {file_name}")
+        sent_message = await app.send_audio(
+            chat_id=callback_query.message.chat.id, 
+            audio=file_name, 
+            caption="audio"
+        )
+        cache[cache_key] = (callback_query.message.chat.id, sent_message.id)
+        os.remove(file_name)
 
 def run_bot():
     logging.info("Запуск бота...")
