00289b65c439ad347333077066f7dbed4033adf4
Grisha <grisha765@tuta.io>
2024-06-29 17:48:37 +0300

    [-] clear detect qulities

--- core/base.py
+++ core/base.py
@@ -1,22 +1,27 @@
 from youtube.download import download_audio, download_video, get_video_info
+
 async def run_youtube():
     url = input("Введите URL видео: ")
     info = await get_video_info(url)
 
-    print("Доступные качества видео:", info)
+    qualities = info["qualities"]
+    print("Доступные качества видео:")
+    for quality in qualities:
+        print(quality)
+    
     choice = input("Выберите качество или введите 'audio' для загрузки только аудио: ")
     
     if choice == 'audio':
         await download_audio(url)
     else:
         try:
-            choice = int(choice)
-            if choice in available_qualities:
-                await download_video(url, choice)
-            else:
-                print("Выбранное качество недоступно!")
+            available_qualities = [quality.replace('p', '').split(' - ')[0] for quality in qualities]
+            
+            choice = str(choice)
+            await download_video(url, choice)
         except ValueError:
             print("Неверный выбор качества!")
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
+
