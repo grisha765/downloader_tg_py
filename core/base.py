fc2025544fe851e8d9cad24a5e4f0d0c8872f051
Grisha <grisha765@tuta.io>
2024-07-01 17:30:54 +0300

    [u] add logging

--- core/base.py
+++ core/base.py
@@ -1,6 +1,9 @@
+from config import logging_config
+logging = logging_config.setup_logging(__name__)
 from youtube.download import download_audio, download_video, get_video_info
 
 async def run_youtube():
+    logging.info("Start base.py...")
     url = input("Введите URL видео: ")
     info = await get_video_info(url)
 
@@ -12,15 +15,17 @@ async def run_youtube():
     choice = input("Выберите качество или введите 'audio' для загрузки только аудио: ")
     
     if choice == 'audio':
-        await download_audio(url)
+        logging.info("Download audio...")
+        file_mame = await download_audio(url)
+        logging.info(f"Audio as {file_mame} uploaded successfully!")
     else:
         try:
-            available_qualities = [quality.replace('p', '').split(' - ')[0] for quality in qualities]
-            
             choice = str(choice)
-            await download_video(url, choice)
+            logging.info("Download video...")
+            file_mame = await download_video(url, choice)
+            logging.info(f"Video as {file_mame} uploaded successfully!")
         except ValueError:
-            print("Неверный выбор качества!")
+            logging.error(f"Wrong choice of quality!")
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
