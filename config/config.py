69c76e940d8882956e8d82117cc82f1bad868ad5
Grisha <grisha765@tuta.io>
2024-07-04 20:30:00 +0300

    [u] update config

--- config/config.py
+++ config/config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config:
-    chrome_path: str = '/bin/chromium'
     log_level: str = "INFO"
     tests: str = "False"
     tg_id: str = '1'
     tg_hash: str = 'b6b154c3707471f5339bd661645ed3d6'
     tg_token: str = 'None'
-
+    notify_timeout: int = 900
+    
     @classmethod
     def load_from_env(cls):
         for key, value in cls.__annotations__.items():
