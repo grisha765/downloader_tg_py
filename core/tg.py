2846c368ed1998d42b9cd45f893431472ba59607
Grisha <grisha765@tuta.io>
2024-07-04 20:30:23 +0300

    [u] addchannel and delchannel routers

--- core/tg.py
+++ core/tg.py
@@ -1,14 +1,23 @@
+import asyncio
 from config.config import Config
 from pyrogram import Client, filters
-from youtube.tg_func import func_message, func_video_selection, func_audio_selection, sponsor_block_toggle
+from youtube.tg_func import func_message, func_video_selection, func_audio_selection, sponsor_block_toggle, add_channel_command, del_channel_command, process_user_channels
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
 app = Client("bot", api_id=Config.tg_id, api_hash=Config.tg_hash, bot_token=Config.tg_token)
 
-@app.on_message(filters.command("sponsor"))
+@app.on_message(filters.command("sponsor") & filters.private)
 async def handle_sponsor_block_toggle(client, message):
     await sponsor_block_toggle(message)
+
+@app.on_message(filters.command("addchannel") & filters.private)
+async def handle_add_channel_command(client, message):
+    await add_channel_command(message)
+
+@app.on_message(filters.command("addchannel") & filters.private)
+async def handle_del_channel_command(client, message):
+    await del_channel_command(message)
 
 @app.on_message(filters.text & filters.private)
 async def handle_message(client, message):
@@ -22,9 +31,18 @@ async def handle_video_selection(client, callback_query):
 async def handle_audio_selection(client, callback_query):
     await func_audio_selection(callback_query, app)
 
+async def periodic_task(app):
+    while True:
+        await process_user_channels(app)
+        await asyncio.sleep(Config.notify_timeout)
+
+async def start_periodic_task(app):
+    await app.start()
+    await periodic_task(app)
+
 def run_bot():
-    logging.info("Запуск бота...")
-    app.run()
+    logging.info("Launching the bot...")
+    app.run(start_periodic_task(app))
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
