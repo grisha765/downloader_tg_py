a737bb2bff0ee5b8aa07bbc32846694001067625
Grisha <grisha765@tuta.io>
2024-07-05 18:24:00 +0300

    [u] update main

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import asyncio
+from db.db import init, close
 from config.config import Config
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
 logging.info(f"Script initialization, logging level: {Config.log_level}")
 
+async def main():
+    from core.tg import start_bot
+    await init()
+    await start_bot()
+    await close()
+
 if __name__ == '__main__':
     if Config.tg_token != 'None':
-        from core.tg import run_bot
-        run_bot()
-    else:
-        from core.base import run_youtube
-        asyncio.run(run_youtube())
+        asyncio.run(main())
