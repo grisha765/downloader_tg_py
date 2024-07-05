f9dc5891c7e55248eae017cc6dc4ac916d8c7eb3
Grisha <grisha765@tuta.io>
2024-07-05 18:22:50 +0300

    [+] init cache func

---
+++ db/cache.py
@@ -0,0 +1,19 @@
+from db.models import Cache
+
+async def get_cache(url, quality):
+    cache_entry = await Cache.filter(url=url, quality=quality).first()
+    if cache_entry:
+        return cache_entry.chat_id, cache_entry.message_id
+    return None, None
+
+async def set_cache(url, quality, chat_id, message_id):
+    cache_entry = await Cache.filter(url=url, quality=quality).first()
+    if cache_entry:
+        cache_entry.chat_id = chat_id
+        cache_entry.message_id = message_id
+        await cache_entry.save()
+    else:
+        await Cache.create(url=url, quality=quality, chat_id=chat_id, message_id=message_id)
+
+if __name__ == "__main__":
+    raise RuntimeError("This module should be run only via main.py")
