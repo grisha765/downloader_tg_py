0c37c84ae264ede6af8407335144ef078db820c1
Grisha <grisha765@tuta.io>
2024-07-05 19:04:57 +0300

    [u] log message

--- db/cache.py
+++ db/cache.py
@@ -12,8 +12,10 @@ async def set_cache(url, quality, chat_id, message_id):
         cache_entry.chat_id = chat_id
         cache_entry.message_id = message_id
         await cache_entry.save()
+        return f"Cache updated for URL: {url}, Quality: {quality}, Chat ID: {chat_id}, Message ID: {message_id}."
     else:
         await Cache.create(url=url, quality=quality, chat_id=chat_id, message_id=message_id)
+        return f"Cache created for URL: {url}, Quality: {quality}, Chat ID: {chat_id}, Message ID: {message_id}."
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
