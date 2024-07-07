29909ec4edeb453b2c73eb95ab4f50e795ebbd8a
Grisha <grisha765@tuta.io>
2024-07-07 18:20:04 +0300

    [u] add db_path and tests

--- config/config.py
+++ config/config.py
@@ -2,11 +2,12 @@ import os
 
 class Config:
     log_level: str = "INFO"
-    tests: str = "False"
     tg_id: str = '1'
     tg_hash: str = 'b6b154c3707471f5339bd661645ed3d6'
     tg_token: str = 'None'
+    db_path: str = ''
     notify_timeout: int = 900
+    tests: str = 'False'
     
     @classmethod
     def load_from_env(cls):
@@ -17,6 +18,12 @@ class Config:
                     setattr(cls, key, int(env_value))
                 else:
                     setattr(cls, key, env_value)
+        cls.update_db_path()
+
+    @classmethod
+    def update_db_path(cls):
+        if not cls.db_path:
+            cls.db_path = f'sqlite://{cls.tg_token[:10]}.db'
 
 Config.load_from_env()
 
