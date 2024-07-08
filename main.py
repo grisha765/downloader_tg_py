06b0bb9f380ed5c52a811db90846d6b2c6f7e81a
Grisha <grisha765@tuta.io>
2024-07-08 20:53:08 +0300

    [f] fix stop bot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import asyncio
+import asyncio, signal
 from db.db import init, close
 from config.config import Config
 from config import logging_config
@@ -7,10 +7,21 @@ logging = logging_config.setup_logging(__name__)
 logging.info(f"Script initialization, logging level: {Config.log_level}")
 
 async def main():
-    from core.tg import start_bot
+    from core.tg import start_bot, stop_bot
     await init()
     await start_bot()
-    await close()
+
+    loop = asyncio.get_running_loop()
+
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGINT, stop.set_result, None)
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    try:
+        await stop
+    finally:
+        await stop_bot()
+        await close()
 
 async def run_tests():
     from autotests.run import run
