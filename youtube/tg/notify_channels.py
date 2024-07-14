df0c873eb6c833b55bc3a0ccf5725c954287514d
Grisha <grisha765@tuta.io>
2024-07-15 01:19:45 +0300

    [r] refactor notify for get id

--- youtube/tg/notify_channels.py
+++ youtube/tg/notify_channels.py
@@ -6,27 +6,27 @@ from youtube.tg.video import download_video_tg
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
-last_sent_urls = {}
+last_sent_ids = {}
 async def process_user_channels(app):
     for user_id, channel_urls in await get_all_channels():
-        if user_id not in last_sent_urls:
-            last_sent_urls[user_id] = {}
+        if user_id not in last_sent_ids:
+            last_sent_ids[user_id] = {}
         for channel_url in channel_urls:
-            new_video_url = await scrap_video_url(channel_url)
-            if new_video_url:
-                if last_sent_urls[user_id].get(channel_url) != new_video_url:
-                    logging.debug(f"{user_id}: New video found on {channel_url}: {new_video_url}")
-                    video_info = await get_video_info(new_video_url)
+            new_video_id = await scrap_video_url(channel_url)
+            if new_video_id:
+                if last_sent_ids[user_id].get(channel_url) != new_video_id:
+                    logging.debug(f"{user_id}: New video found on {channel_url}: {new_video_id}")
+                    video_info = await get_video_info(new_video_id)
                     message_text = f"""
 **Название**: {video_info['name']}
 **Автор**: {video_info['author']}
 **Дата выхода**: {video_info['date']}
 **Продолжительность**: {video_info['duration']}
-**Ссылка**: {new_video_url}
+**Ссылка**: https://www.youtube.com/watch?v={new_video_id}
                 """
                     message = await app.send_photo(user_id, video_info['thumbnail'], caption=message_text)
-                    await download_video_tg(app, new_video_url, '720', message, user_id)
-                    last_sent_urls[user_id][channel_url] = new_video_url
+                    await download_video_tg(app, new_video_id, '720', message, user_id)
+                    last_sent_ids[user_id][channel_url] = new_video_id
                 else:
                     logging.debug(f"User {user_id} already received the latest video from {channel_url}.")
             else:
