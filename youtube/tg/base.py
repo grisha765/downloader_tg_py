1b2ed78ed3abc2f02abf751cba8d6e1271917850
Grisha <grisha765@tuta.io>
2024-07-15 01:19:22 +0300

    [u] use get id in base

--- youtube/tg/base.py
+++ youtube/tg/base.py
@@ -3,6 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from youtube.download import get_video_info
 from youtube.tg.video import download_video_tg
 from youtube.tg.audio import download_audio_tg
+from youtube.get_id import get_url_id
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
@@ -14,23 +15,24 @@ def create_quality_buttons(qualities):
     buttons.append([InlineKeyboardButton("Audio", callback_data='audio')])
     return InlineKeyboardMarkup(buttons)
 
-url_list = {}
+url_id_list = {}
 async def func_message(message):
     text = message.text
     user_id = message.from_user.id
     logging.debug(f"{user_id}: Received message: {text}")
 
     url_pattern = r'(https?://(?:www\.|m\.)?youtube\.com/watch\?v=[\w-]+|https?://youtu\.be/[\w-]+)'
-    urls = re.findall(url_pattern, text)
+    url_message = "".join(re.findall(url_pattern, text))
     
-    logging.debug(f"Found URLs: {urls}")
+    logging.debug(f"Found URLs: {url_message}")
 
-    if urls:
+    if url_message:
         info_message = await message.reply_text(f"Search video by url...")
-        for url in urls:
-            url_list[user_id] = url
+        for url in url_message:
+            url_id = get_url_id(str(url_message))
+            url_id_list[user_id] = url_id
             try:
-                video_info = await get_video_info(url)
+                video_info = await get_video_info(url_id)
                 logging.debug(f"Video info for {url}: {video_info}")
             except:
                 await info_message.edit_text("Error retrieving data from url.")
@@ -58,22 +60,22 @@ async def func_video_selection(callback_query: CallbackQuery, app):
     
     logging.debug(f"{user_id}: Selected quality: {quality}")
 
-    url = url_list.get(user_id)
-    if not url:
-        await callback_query.answer("URL not found.")
+    url_id = url_id_list.get(user_id)
+    if not url_id:
+        await callback_query.answer("URL ID not found.")
         return
-    await download_video_tg(app, url, quality, callback_query.message, user_id)
+    await download_video_tg(app, url_id, quality, callback_query.message, user_id)
 
 async def func_audio_selection(callback_query: CallbackQuery, app):
     user_id = callback_query.from_user.id
     quality = "audio"
     logging.debug(f"{user_id}: Audio option selected")
 
-    url = url_list.get(user_id)
-    if not url:
+    url_id = url_id_list.get(user_id)
+    if not url_id:
         await callback_query.answer("URL not found.")
         return
-    await download_audio_tg(app, url, quality, callback_query.message, user_id)
+    await download_audio_tg(app, url_id, quality, callback_query.message, user_id)
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
