155e491f4ba534ffc74cc28d5bedda34b6a6b809
Grisha <grisha765@tuta.io>
2025-04-07 00:59:57 +0300

    [u] getting video info and generating buttons with quality

--- bot/funcs/commands.py
+++ bot/funcs/commands.py
@@ -1,5 +1,6 @@
-import re
-from bot.funcs.youtube import download_video
+import re, pyrogram.types
+from bot.funcs.youtube import download_video, get_video_info
+from bot.core.classes import Common
 from bot.config import logging_config
 logging = logging_config.setup_logging(__name__)
 
@@ -8,8 +9,9 @@ async def start_command(_, message):
     await message.reply_text("Hello world.")
 
 
-async def download_command(_, message):
+async def get_video_command(_, message):
     text = message.text
+    message_id = message.id
     user_id = message.from_user.id
 
     logging.debug(f"{user_id}: Received message: {text}")
@@ -17,9 +19,33 @@ async def download_command(_, message):
     url_message = "".join(re.findall(url_pattern, text))
     
     if url_message:
-        logging.debug(f"Found URLs: {url_message}")
-        video = download_video(url_message)
-        await message.reply_video(video, caption="Here is your video!")
+        Common.select_video[message_id] = url_message
+        try:
+            quality_dict = get_video_info(url_message)
+        except Exception as e:
+            message.reply(f"Error retrieving video info: {e}")
+            return
+
+        buttons = []
+        for quality, size in quality_dict.items():
+            button_text = f"{quality}p - {size} MB"
+            button = pyrogram.types.InlineKeyboardButton(text=button_text, callback_data=f"quality_{quality}")
+            buttons.append([button])
+
+        reply_markup = pyrogram.types.InlineKeyboardMarkup(buttons)
+
+        await message.reply("Select video quality:", reply_markup=reply_markup)
+
+
+async def download_video_command(_, callback_query):
+    quality = callback_query.data.split("_", 1)[1]
+    message_id = callback_query.message.id - 1
+    url_message = Common.select_video.get(message_id)
+    assert url_message
+    await callback_query.answer(f"You selected {quality}p quality!")
+    logging.debug(f"Found URLs: {url_message}")
+    video = download_video(url_message, quality)
+    await callback_query.message.reply_video(video, caption="Here is your video!")
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
--- bot/funcs/youtube.py
+++ bot/funcs/youtube.py
@@ -5,7 +5,36 @@ from bot.config import logging_config
 logging = logging_config.setup_logging(__name__)
 
 
-def download_video(url: str) -> BytesIO:
+def get_video_info(url: str) -> dict:
+    ydl_opts = {
+        'quiet': True,
+        'noplaylist': True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+
+    quality_size_dict = {}
+    assert info
+    for fmt in info.get('formats', []):
+        height = fmt.get('height')
+        if height is None:
+            continue
+        size = fmt.get('filesize') or fmt.get('filesize_approx')
+        if size is None:
+            continue
+        if height in quality_size_dict:
+            quality_size_dict[height] = max(quality_size_dict[height], size)
+        else:
+            quality_size_dict[height] = size
+
+    quality_size_mb = {height: round(size / (1024 * 1024), 2)
+                       for height, size in quality_size_dict.items()}
+
+    sorted_quality_size_mb = dict(sorted(quality_size_mb.items()))
+    return sorted_quality_size_mb
+
+
+def download_video(url: str, quality: str) -> BytesIO:
     info_opts = {
         'quiet': True,
         'noplaylist': True,
@@ -14,7 +43,8 @@ def download_video(url: str) -> BytesIO:
         info = ydl.extract_info(url, download=False)
     
     native_mp4_available = False
-    for fmt in info['formats']:
+    assert info
+    for fmt in info.get('formats', []):
         ext = fmt.get('ext')
         logging.debug(f"Video format: {ext}")
         if ext == 'mp4':
@@ -22,19 +52,21 @@ def download_video(url: str) -> BytesIO:
             break
 
     with tempfile.TemporaryDirectory() as tmpdirname:
+        logging.debug(f"Temp dir {tmpdirname} avalible")
         outtmpl = str(Path(tmpdirname) / '%(id)s.%(ext)s')
         
         if native_mp4_available:
+            logging.debug("Use mp4 for video")
             ydl_opts = {
                 'outtmpl': outtmpl,
-                'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
+                'format': f"bestvideo[ext=mp4][height={quality}]+bestaudio[ext=m4a]/mp4",
                 'quiet': True,
                 'noplaylist': True,
             }
         else:
             ydl_opts = {
                 'outtmpl': outtmpl,
-                'format': 'bestvideo+bestaudio/best',
+                'format': f"bestvideo[height={quality}]+bestaudio/best",
                 'quiet': True,
                 'noplaylist': True,
                 'postprocessors': [{
