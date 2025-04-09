9a857f5d234f4410149910482e4b62384f06aa78
Grisha <grisha765@tuta.io>
2025-04-09 04:12:24 +0300

    [r] now set_qualitys does not overwrite all data

--- bot/db/cache_qualitys.py
+++ bot/db/cache_qualitys.py
@@ -2,22 +2,6 @@ from typing import Dict
 from .models import CacheQuality
 
 
-async def set_qualitys(url: str, qualitys: Dict[int, float]):
-    await CacheQuality.filter(url=url).delete()
-
-    for resolution, size in qualitys.items():
-        await CacheQuality.create(
-            url=url,
-            resolution=resolution,
-            size=size
-        )
-
-
-async def get_qualitys(url: str) -> Dict[int, float]:
-    records = await CacheQuality.filter(url=url).values("resolution", "size")
-    return {record["resolution"]: record["size"] for record in records}
-
-
 async def set_quality_size(url: str, quality: int, size: float):
     obj, created = await CacheQuality.get_or_create(
         url=url,
@@ -27,6 +11,25 @@ async def set_quality_size(url: str, quality: int, size: float):
     if not created:
         obj.size = size
         await obj.save()
+
+
+async def set_qualitys(url: str, qualitys: Dict[int, float]):
+    for resolution, size in qualitys.items():
+        await set_quality_size(url, resolution, size)
+
+
+async def get_qualitys(url: str) -> Dict[int, float]:
+    records = (
+        await CacheQuality
+        .filter(url=url)
+        .order_by("resolution")
+        .values("resolution", "size")
+    )
+    
+    result = {}
+    for record in records:
+        result[record["resolution"]] = record["size"]
+    return result
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
