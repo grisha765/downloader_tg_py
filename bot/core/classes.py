79abb6660e2ceed7d15abf4f639323d67466c9e0
Grisha <grisha765@tuta.io>
2025-06-20 03:31:30 +0300

    [u] user semaphore

--- bot/core/classes.py
+++ bot/core/classes.py
@@ -1,7 +1,9 @@
-import yt_dlp, httpx
+import yt_dlp, httpx, asyncio
+from collections import defaultdict
 
 class Common:
     select_video = {}
+    user_semaphores = defaultdict(lambda: asyncio.BoundedSemaphore(5))
     user_tasks = {}
     youtube = yt_dlp.YoutubeDL
     http = httpx.AsyncClient()
